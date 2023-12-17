6fb235b52cabca2ae069c9a53abb20ad43960981
Darian Puka <pukadarian@gmail.com>
2023-12-16 22:30:41 -0600

    adding all code

---
+++ README.md
@@ -0,0 +1,30 @@
+# Friend Trivia Game
+
+This is a trivia game to play with friends.
+
+## How to Play
+
+To start, each player must include their name in the config.
+
+When the program is run, a file will be generated for each player.
+Each player should be provided their own file and should keep the contents
+secret.
+
+Players will take turns presenting a set of prompts (for example, 4 at a time). To 
+present, a player will answer the prompt and pull up an image that represents the
+answer that they chose. The player shows this image to the rest of the players and
+has the rest of the players either provide a rating or answer the question. This
+can be done on a countdown.
+
+Once every player has answered the prompt, the player can reveal what the prompt
+was. They will receive a point if the answer the player thought of matches the responses
+to the prompt.
+
+## Example
+
+Assuming a game with 3 players and 1 prompt for each player:
+
+Player 1 has the prompt: "A music artist / band that everybody would rate a 8-10"
+Player 1 decides to answer with "All Time Low". They ask the rest of the players what they would rate the band.
+Assuming every player rated the band with an 8/9/10, Player 1 will receive a point. Otherwise, they would continue to the 
+next question.
---
+++ app.py
@@ -0,0 +1,51 @@
+
+import random
+import json
+
+with open("config.json", "r", encoding="utf-8") as config_file:
+    config = json.loads(config_file.read())
+
+for player in config["People"]:
+    with open(f"{player}.txt", "w", encoding="utf-8") as player_file:
+        for question_num in range(config["NumberOfQuestions"]):
+            topic_dict = config["Topics"][random.randint(0, len(config["Topics"])-1)]
+            topic = topic_dict["topic"]
+            if topic_dict.get("modifier_chance") and random.random() < topic_dict["modifier_chance"]:
+                topic += f" {topic_dict['modifiers'][random.randint(0, len(topic_dict['modifiers'])-1)]}"
+
+
+            people = config["People"].copy()
+            
+            # Determine the people involved
+            if random.random() < config["ChanceFor_Everybody"]:
+                person = "everybody"
+            elif random.random() < config["ChanceFor_Nobody"]:
+                person = "nobody"
+            elif random.random() < config["ChanceFor_OnlyOnePerson"]:
+                person = "everybody"
+
+            else:
+                person = ""
+                if random.random() < config["ChanceFor_OnlyPeople"]:
+                    person = "only"
+                
+                rand_person = people.pop(random.randint(0, len(people)-1))
+                person = f"{person} {rand_person}".strip()
+                while random.random() < config["ChanceFor_ExtraPerson"] and len(people):
+                    rand_person = people.pop(random.randint(0, len(people)-1))
+                    person = f"{person}+{rand_person}"
+                
+                if not len(people):
+                    person = "everybody"
+
+            suffix = topic_dict["suffixes"][random.randint(0, len(topic_dict["suffixes"])-1)]
+            suffix = suffix.replace("%1", person)
+            if "+" in person:
+                suffix = suffix.replace("%2", "have")
+            else:
+                suffix = suffix.replace("%2", "has")
+            
+            if suffix.endswith("rate"):
+                suffix += f" a {config['Ratings'][random.randint(0, len(config['Ratings'])-1)]}" 
+
+            player_file.write(f"{topic} {suffix}\n")
